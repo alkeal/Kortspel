db1db3c57aa6e8f7cd785f874cd91e06d977adeb
a <yhma22.alkeal@folkuniversitetet.nu>
2022-11-15 12:39:28 +0100

    Nu ändras korten när man trycker på knappen.

--- app/src/main/java/com/example/kortspel/MainActivity.kt
+++ app/src/main/java/com/example/kortspel/MainActivity.kt
@@ -9,11 +9,31 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import java.util.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
 
   lateinit var textAppNameView: TextView
+  lateinit var imageView: ImageView
+
+  val cardImages : IntArray = intArrayOf(
+
+      R.drawable.card1,
+              R.drawable.card2,
+      R.drawable.card3,
+      R.drawable.card4,
+      R.drawable.card5,
+      R.drawable.card6,
+      R.drawable.card7,
+      R.drawable.card8,
+      R.drawable.card9,
+      R.drawable.card10,
+      R.drawable.card11,
+      R.drawable.card12,
+      R.drawable.card13,
+  )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +45,14 @@ class MainActivity : AppCompatActivity() {
 
          val buttonHigher = findViewById<Button>(R.id.buttonHigher)
 
+        imageView = findViewById(R.id.imageCardView)
+
         buttonLower.setOnClickListener {
             val intent = Intent(this,AnswerActivity::class.java)
-
+           val random = kotlin.random.Random
+            imageView.setImageResource(cardImages[random.nextInt(cardImages.size)])
             startActivity(intent)
-            buttonPressed()
+            //buttonPressed()
         }
 
         buttonHigher.setOnClickListener {
@@ -45,8 +68,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonPressed (){
-
-
 
 
 
