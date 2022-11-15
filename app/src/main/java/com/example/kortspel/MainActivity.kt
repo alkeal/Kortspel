05c06c1cbef4e10ad44a0b9d06ec67ae1dc21d2d
a <yhma22.alkeal@folkuniversitetet.nu>
2022-11-15 11:51:02 +0100

    Fixade spelkort till drawables

--- app/src/main/java/com/example/kortspel/MainActivity.kt
+++ app/src/main/java/com/example/kortspel/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.kortspel
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
          textAppNameView = findViewById(R.id.textAppNameView)
 
          val buttonLower = findViewById<Button>(R.id.buttonLower)
@@ -23,15 +26,31 @@ class MainActivity : AppCompatActivity() {
          val buttonHigher = findViewById<Button>(R.id.buttonHigher)
 
         buttonLower.setOnClickListener {
-            val intent = Intent(this,AnswerActivity)
+            val intent = Intent(this,AnswerActivity::class.java)
+
             startActivity(intent)
+            buttonPressed()
         }
 
         buttonHigher.setOnClickListener {
-            val intent = Intent(this,AnswerActivity)
-            startActivity(intent)
+            val intent = Intent(this,AnswerActivity::class.java)
 
+            startActivity(intent)
+            buttonPressed()
         }
+
+
+
+
+    }
+
+    fun buttonPressed (){
+
+
+
+
+
+
 
 
 
